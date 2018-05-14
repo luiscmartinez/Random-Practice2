2b496e032af5f03c1258d02f7b1529f0059302a6
Luis <luis@Luiss-MacBook-Air.local>
2018-05-14 14:01:19 -0700

    added project

---
+++ app/index.js
@@ -0,0 +1,11 @@
+var React = require('react')
+var ReactDom = require('react-dom')
+require('./index.css')
+
+class App extends React.Component {
+  render () {
+    return <h1>Hello Niggers!</h1>
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('App'))
