eedc2f00087e8216709bda409937ddc2fe60d2f1
Luis <luis@Luiss-MacBook-Air.local>
2018-05-14 14:08:04 -0700

    Created a folder for all components & export & imported it to index.js

---
+++ app/components/App.js
@@ -0,0 +1,9 @@
+var React = require('react')
+
+class App extends React.Component {
+  render () {
+    return <h1>Hello Niggers!</h1>
+  }
+}
+
+module.exports = App
--- app/index.js
+++ app/index.js
@@ -1,11 +1,6 @@
 var React = require('react')
 var ReactDom = require('react-dom')
 require('./index.css')
-
-class App extends React.Component {
-  render () {
-    return <h1>Hello Niggers!</h1>
-  }
-}
+var App = require('./components/App')
 
 ReactDom.render(<App />, document.getElementById('App'))
